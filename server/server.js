1c47a4a8244260f5a4ee328e3e03186c5589da85
charleneyt <81109925+charleneyt@users.noreply.github.com>
2021-12-02 13:58:18 -0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,12 @@ app.get('/rescues', routes.rescues)
 // Route 2 - register as GET 
 app.get('/search/rescues', routes.search_rescues)
 
+// Route d - register as GET 
+app.get('/top10/:type', routes.top10)
+
+// Route e - register as GET 
+app.get('/compare/:username', routes.compare)
+
 app.listen(config.server_port, () => {
     console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
 });
