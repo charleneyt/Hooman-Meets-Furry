6dce07556f513bfa8161e2350293965c9cc46fe6
Tiffany <tiffanychien9@gmail.com>
2021-12-02 15:54:43 -0500

    pet search engine routes and server

--- server/server.js
+++ server/server.js
@@ -1,40 +1,45 @@
-const express = require('express');
-const mysql      = require('mysql');
+const express = require("express");
+const mysql = require("mysql");
 
-
-const routes = require('./routes')
-const config = require('./config.json')
-const cors = require('cors');
-
+const routes = require("./routes");
+const config = require("./config.json");
+const cors = require("cors");
 
 const app = express();
-app.use(cors({
-    origin: '*'
-}));
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-// Route 1 - register as GET 
-app.get('/rescues', routes.rescues)
-
-// Route 2 - register as GET 
-app.get('/search/rescues', routes.search_rescues)
-
-// Route d - register as GET 
-app.get('/top10/:type', routes.top10)
-
-// Route e - register as GET 
-app.get('/compare/:username', routes.compare)
+// Route a
+app.get("/petSearch/pet", routes.pet_search);
 
 // Route b - register as GET
-app.get('/recommend', routes.recommend)
+app.get("/recommend", routes.recommend);
+
+// Route c - register as GET
+app.get("/rescues", routes.rescues);
+
+// Route d - register as GET
+app.get("/search/rescues", routes.search_rescues);
+
+// Route d - register as GET
+app.get("/top10/:type", routes.top10);
+
+// Route e - register as GET
+app.get("/compare/:username", routes.compare);
 
 // Route f - register as GET
-app.get('/get_similar', routes.get_similar)
+app.get("/get_similar", routes.get_similar);
 
 // Route g - register as GET
-app. get('/user_login', routes.user_login)
+app.get("/user_login", routes.user_login);
 
 app.listen(config.server_port, () => {
-    console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
+  console.log(
+    `Server running at http://${config.server_host}:${config.server_port}/`
+  );
 });
 
 module.exports = app;
