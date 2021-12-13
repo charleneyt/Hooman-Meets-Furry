b7d15864eebd04bc11b25770d3797fe7e2157e7d
Tiffany <tiffanychien9@gmail.com>
2021-12-13 13:48:45 -0500

    pet compare page implemented

--- src/Pages/PetComparePage.jsx
+++ src/Pages/PetComparePage.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import CompareTable from "../components/CompareTable/CompareTable";
+import Box from "@mui/material/Box";
+import { getCompare } from "../fetcher";
 
-export default function PetCompare() {
-  return <div>PetCompare!</div>;
+export default function PetCompare(props) {
+  const{username} = props;
+  const[data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+    getCompare(username).then(resp => resp.json()).then(resp => {
+      setData(resp.results)
+    })
+  }, [username]);
+
+  return <div>PetCompare!
+    <Box maxWidth={800}><CompareTable data={data}/></Box>
+  </div>;
 }
