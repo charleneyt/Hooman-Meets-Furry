7e10e2e894aec8459a47f0c5cbb42b674cf78711
yingliu319 <77655830+yingliu319@users.noreply.github.com>
2021-12-14 18:09:27 -0800

    Update CompareTable.jsx font family

--- src/components/CompareTable/CompareTable.jsx
+++ src/components/CompareTable/CompareTable.jsx
@@ -12,9 +12,12 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: theme.palette.common.black,
     color: theme.palette.common.white,
+    fontSize: 25,
+    fontFamily: "Dongle"
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
+    fontSize: 25,
+    fontFamily: "Dongle"
   },
 }));
 
